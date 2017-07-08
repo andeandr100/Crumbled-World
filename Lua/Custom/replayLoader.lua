f31c4a6c83090e540f1f9fdf6d5e62adbaab7698
Anders Andreasson <andeandr100@gmail.com>
2017-07-08 13:52:40 +0200

    add replayLoader

---
+++ Lua/Custom/replayLoader.lua
@@ -0,0 +1,50 @@
+require("Game/mapInfo.lua")
+require("Game/campaignData.lua")
+require("Menu/MainMenu/mapInformation.lua")
+
+local levelInfo = MapInfo.new()
+local campaignData = CampaignData.new()
+local files = campaignData.getMaps()
+local filePath = Core.getGlobalBillboard("highScoreReplay"):getString("filePath")
+local difficulty = Core.getGlobalBillboard("highScoreReplay"):getInt("difficulty")
+local mapFile = File(filePath)
+
+function getMapIndex(filePath)
+	for i=1, #files do	
+		local file = files[i].file
+		if file:isFile() and file:getPath()==filePath then
+			return i
+		end
+	end
+	return 0
+end
+
+function create()
+	
+	
+	local mNum = getMapIndex(filePath)
+	
+	levelInfo.setIsCampaign(true)
+	levelInfo.setMapNumber(mNum)
+	levelInfo.setSead(files[mNum].sead)
+	levelInfo.setMapFileName(filePath)
+	levelInfo.setMapName(mapFile:getName())
+	if mapFile:isFile() then
+		local mapInfo = MapInformation.getMapInfoFromFileName(mapFile:getName(), mapFile:getPath())
+		if mapInfo then
+			levelInfo.setIsCartMap(mapInfo.gameMode=="Cart")
+		end
+		if mapInfo then
+			levelInfo.setChangedDifficultyMax(mapInfo.difficultyIncreaseMax)
+			levelInfo.setChangedDifficultyMin(mapInfo.difficultyIncreaseMin)
+			levelInfo.setWaveCount(mapInfo.waveCount)
+		end
+	end
+	levelInfo.setLevel(difficulty)
+	
+	return false
+end
+
+function update()
+	return false
+end
