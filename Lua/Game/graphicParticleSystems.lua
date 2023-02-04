dedaeca51c7f76df8c1e9db04eb8a0b3661d613b
andeandr100 <andeandr100@gmail.com>
2023-02-04 19:04:41 +0100

    updated default particle effects

--- Lua/Game/graphicParticleSystems.lua
+++ Lua/Game/graphicParticleSystems.lua
@@ -5,30 +5,35 @@ function GraphicParticleSystems.new(portalSize)
 	local bilboardParticleEffects = Core.getGameSessionBillboard("ParticleEffectStorage")
 	
 	function self.createTowerElectricEffect()
-		local particleEffect = GraphicParticleSystem.new(25,1)
-		particleEffect:setRenderBlendMode(GL_Blend.SRC_ALPHA, GL_Blend.ONE)
-		
-
-		for i=1, particleEffect:getMaxParticles() do 
-			local position = math.randomVec3():normalizeV() * math.randomFloat(-0.24,0.24)
-			local uvCoord = Vec2(0.75,0.0) + Vec2( math.randomFloat() > 0.5 and 0.125 or 0.0, math.randomFloat() > 0.5 and 0.125 or 0.0 )
-			local startColor = Vec4(0.8, 1.2, 2.0, 0.75)
-			local finalColor = Vec4(0.35, 0.50, 1.2, 0.0)
-			local startSize = math.randomFloat(0.12,0.16)
-			local finalSize = math.randomFloat(0.5,0.65)
-			local velocity = position:normalizeV() * math.randomFloat(0.0,0.1)
+		local particleEffect = bilboardParticleEffects:getSceneNode("TowerElectricCenter")
+		if particleEffect ~= nil then
+			particleEffect = GraphicParticleSystem.new(particleEffect)		
+		else
+			particleEffect = GraphicParticleSystem.new(25,1)
+			particleEffect:setRenderBlendMode(GL_Blend.SRC_ALPHA, GL_Blend.ONE)
+			
 	
-			particleEffect:addparticle( position,velocity,uvCoord, startColor, finalColor, startSize, finalSize, (i-1)/particleEffect:getMaxParticles() )
-
-		end
-		particleEffect:compile()
-		particleEffect:setLocalPosition(Vec3())
+			for i=1, particleEffect:getMaxParticles() do 
+				local position = math.randomVec3():normalizeV() * math.randomFloat(-0.24,0.24)
+				local uvCoord = Vec2(0.75,0.0) + Vec2( math.randomFloat() > 0.5 and 0.125 or 0.0, math.randomFloat() > 0.5 and 0.125 or 0.0 )
+				local startColor = Vec4(0.8, 1.2, 2.0, 0.75)
+				local finalColor = Vec4(0.35, 0.50, 1.2, 0.0)
+				local startSize = math.randomFloat(0.12,0.16)
+				local finalSize = math.randomFloat(0.5,0.65)
+				local velocity = position:normalizeV() * math.randomFloat(0.0,0.1)
 		
+				particleEffect:addparticle( position,velocity,uvCoord, startColor, finalColor, startSize, finalSize, (i-1)/particleEffect:getMaxParticles() )
+	
+			end
+			particleEffect:compile()
+			particleEffect:setLocalPosition(Vec3())
+			
+			bilboardParticleEffects:setSceneNode("TowerElectricCenter", particleEffect:toSceneNode() )
+		end
 		return particleEffect
 	end
 	
-	function self.createTowerFireCenter()
-		
+	function self.createTowerFireCenter()		
 		local particleEffect = bilboardParticleEffects:getSceneNode("TowerFlameCenter")
 		if particleEffect ~= nil then
 			particleEffect = GraphicParticleSystem.new(particleEffect)		
@@ -53,6 +58,35 @@ function GraphicParticleSystems.new(portalSize)
 			particleEffect:setLocalPosition(Vec3())
 			
 			bilboardParticleEffects:setSceneNode("TowerFlameCenter", particleEffect:toSceneNode() )
+		end
+		return particleEffect
+	end
+	
+	function self.createSwarmBall()		
+		local particleEffect = bilboardParticleEffects:getSceneNode("SwarmBall")
+		if particleEffect ~= nil then
+			particleEffect = GraphicParticleSystem.new(particleEffect)		
+		else
+			particleEffect = GraphicParticleSystem.new(7,1)
+			particleEffect:setRenderBlendMode(GL_Blend.SRC_ALPHA, GL_Blend.ONE)
+			
+	
+			for i=1, particleEffect:getMaxParticles() do 
+				local position = Vec3(math.randomFloat(-1.0,1.0),0,math.randomFloat(-1.0,1.0)):normalizeV() * math.randomFloat(-0.0375,0.0375)
+				local uvCoord = Vec2(0.0,0.75) + Vec2(0, math.randomFloat() > 0.5 and 0.125 or 0.0 )
+				local startColor = Vec4(1.1, 0.3, 0.1, 0.5)
+				local finalColor = Vec4(1.1, 0.6, 0.2, 0.3)
+				local startSize = math.randomFloat(0.09,0.098)
+				local finalSize = math.randomFloat(0.13,0.15)
+				local velocity = Vec3()
+		
+				particleEffect:addparticle( position, velocity, uvCoord, startColor, finalColor, startSize, finalSize, (i-1)/particleEffect:getMaxParticles() )
+	
+			end
+			particleEffect:compile()
+			particleEffect:setLocalPosition(Vec3())
+			
+			bilboardParticleEffects:setSceneNode("SwarmBall", particleEffect:toSceneNode() )
 		end
 		return particleEffect
 	end
